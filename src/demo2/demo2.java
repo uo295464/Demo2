ef5de80aa41abff476b4193d7783784c7ba0bbe0
yago-vasco <yago.rc.04@gmail.com>
2025-02-06 18:34:46 +0100

    master

--- src/demo2/demo2.java
+++ src/demo2/demo2.java
@@ -4,6 +4,7 @@ public class demo2 {
 
 	public static void main(String[] args) {
 		System.out.println("hello World");
+		System.out.println("hello World");
 
 	
 		System.out.println("Hola desde la rama");
